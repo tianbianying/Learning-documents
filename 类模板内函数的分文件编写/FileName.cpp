88be9d0faa3ca57dc09572d1d454d5c950e2fdc7
Excell <3061266318qq.com>
2023-06-04 22:43:00 +0800

    添加项目文件。

---
+++ 类模板内函数的分文件编写/FileName.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+# include "Person.hpp"
+using namespace std;
+
+void test01()//因为类模板内部的函数只有在调用时才会实现，所以普通的分文件编写会导致cpp文件不会被编译
+{			//所以有两种解决方法1.包含cpp文件 2.是把所有文件都写进hpp文件hpp约定俗成的是类模板文件  第二种是更常用的。
+	Person<string, int>p("李白", 2000);
+	p.Print();
+}
+
+int main()
+{
+	test01();
+	system("pause");
+	return 0;
+}
---
+++ 类模板内函数的分文件编写/Person.hpp
@@ -0,0 +1,26 @@
+#pragma once
+# include <iostream>
+using namespace std;
+
+template<class T1, class T2>
+class Person
+{
+public:
+	Person(T1 name, T2 age);
+	void Print();
+	T1 name;
+	T2 age;
+};
+
+template<class T1, class T2>
+Person<T1, T2>::Person(T1 name, T2 age)
+{
+	this->name = name;
+	this->age = age;
+}
+
+template<class T1, class T2>
+void Person<T1, T2>::Print()
+{
+	cout << "ÐÕÃû£º" << this->name << " ÄêÁä£º" << endl;
+}
