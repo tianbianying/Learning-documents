1cd804ce7967b7fd43263edc6f7da5b82ce75752
Excell <3061266318qq.com>
2023-06-04 22:51:57 +0800

    测试

--- 类模板内函数的分文件编写/Person.hpp
+++ 类模板内函数的分文件编写/Person.hpp
@@ -12,12 +12,14 @@ public:
 	T2 age;
 };
 
+
 template<class T1, class T2>
 Person<T1, T2>::Person(T1 name, T2 age)
 {
 	this->name = name;
 	this->age = age;
 }
+
 
 template<class T1, class T2>
 void Person<T1, T2>::Print()
